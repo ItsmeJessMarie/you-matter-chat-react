f5fa2fd5467ad0fc2028a428ce1ca9fbb0a5dbfa
ItsmeJessMarie <jboll262@gmail.com>
2023-12-07 14:41:49 -0600

    Created the React and React routes properties for YouMatter Chat React website
    Created the base foundation for React website.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
